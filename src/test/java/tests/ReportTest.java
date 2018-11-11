5861ff7a67377b8d307819e7215d7deebf707621
SergeNikulin <sergey.nikulin@gost-group.com>
2018-11-11 15:31:42 +0300

    формирование отчета по объекту

--- src/test/java/tests/ReportTest.java
+++ src/test/java/tests/ReportTest.java
@@ -5,33 +5,13 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-
-import javax.annotation.Resource;
-import java.io.IOException;
+import pages.BaseReport;
 
 import static org.junit.Assert.assertEquals;
 
 
-public class IntersectionReportTest {
-
-    @Before
-    public void authorization() throws Exception {
-        Unirest.setDefaultHeader("Content-Type", "application/json");
-        String login = "https://egiptest.mos.ru/egip/login";
-        String bodyPost = "{  \n" +
-                "    \"login\": \"sys\",\n" +
-                "    \"password\": \"123456\"\n" +
-                "}";
-
-        Unirest.post(login)
-                .body(bodyPost)
-                .asJson();
-
-    }
-
+public class ReportTest extends BaseReport {
 
     @Test
     public void intersectionReport() throws Exception {
@@ -58,10 +38,32 @@ public class IntersectionReportTest {
         assertEquals("IN_PROGRESS", jsonResponseTask.getBody().getObject().getJSONObject("data").get("status"));
     }
 
-    @After
-    public void shutdown() throws IOException {
-        Unirest.shutdown();
+    @Test
+    public void reportOnTheObject() throws Exception{
+        String url = "https://egiptest.mos.ru/egip/services/reports/feature";
+        JtwigTemplate templateBody = JtwigTemplate.classpathTemplate("reportOnTheObject.json");
+        JtwigModel model = JtwigModel.newModel()
+                .with("layerId", "15640542")
+                .with("featureId", "15638047");
+        HttpResponse<JsonNode> jsonResponse = Unirest.post(url)
+                .body(templateBody.render(model))
+                .asJson();
+        assertEquals("OK", jsonResponse.getBody().getObject().get("result"));
+        assertEquals("FEATURE_REPORT", jsonResponse.getBody().getObject().getJSONObject("data").get("type"));
+        assertEquals("SCHEDULED", jsonResponse.getBody().getObject().getJSONObject("data").get("status"));
+
+        //Получили ID задачи и проверям что она запущена и работает.
+        String taskId = jsonResponse.getBody().getObject().getJSONObject("data").get("id").toString();
+
+        String urlTask = "https://egiptest.mos.ru/egip/services/tasks/{Id_task}/result";
+        HttpResponse<JsonNode> jsonResponseTask = Unirest.get(urlTask)
+                .routeParam("Id_task", taskId)
+                .asJson();
+        assertEquals("OK", jsonResponseTask.getBody().getObject().get("result"));
+        assertEquals("IN_PROGRESS", jsonResponseTask.getBody().getObject().getJSONObject("data").get("status"));
     }
+
+
 }
 
 
